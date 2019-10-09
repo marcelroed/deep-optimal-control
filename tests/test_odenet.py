490e71983acf8f9bbd454deaeea4af45173fa0ff
marod <marod@stud.ntnu.no>
2019-10-10 01:42:50 +0200

    MNIST test stub

---
+++ tests/test_odenet.py
@@ -0,0 +1,12 @@
+from src.odenet.odenet import ODENet
+from src.data.load_data import get_mnist
+
+
+def test_mnist():
+    """Run MNIST through the ODE-net"""
+    # Get MNIST data
+    train, test = get_mnist(32, 32)
+
+    print(train.data[0])
+
+    net = ODENet()
