0ba354b7c4e3128f9c8bacda28f04e67d0b26828
marod <marod@stud.ntnu.no>
2019-10-21 20:54:27 +0200

    Classifier structure, support mini-batches, evaluation function for prediction accuracy

--- src/odenet/odenet.py
+++ src/odenet/odenet.py
@@ -1,10 +1,13 @@
+from typing import *
 from tqdm import trange
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from src.settings import *
+from torch.utils.data import DataLoader
 import numpy as np
-from typing import *
+
+
+from src.settings import *
 
 
 class ODENet(nn.Module):
@@ -12,7 +15,8 @@ class ODENet(nn.Module):
     ODE inspired neural network architecture.
     Note that the shape of the input is the same as the output for every layer.
     """
-    def __init__(self, input_shape: Tuple[int], layers: int, rk_method: str = 'euler'):
+    def __init__(self, input_shape: Tuple[int, ...], layers: int, reduce_to: Optional[Union[int, Tuple[int, ...]]] = None,
+                 rk_method: str = 'euler'):
         """
         Args:
             input_shape (Tuple[int]): Shape of the input vector
@@ -22,12 +26,14 @@ class ODENet(nn.Module):
         self.delta_t = nn.Parameter(torch.full((layers, ), 1))
         self.input_shape = input_shape
         self.input_size: int = np.prod(input_shape)
-        self.layers = [nn.Linear(self.input_size, self.input_size) for i in range(5)] + [nn.Linear(self.input_size, 1)]
+        self.layers = [nn.Linear(self.input_size, self.input_size) for _ in range(layers)]
+        if reduce_to is not None:
+            self.layers.append(nn.Linear(self.input_size, np.prod(reduce_to)))
 
         for i, layer in enumerate(self.layers):
             setattr(self, f'{layer.__class__.__name__}{i}', layer)
 
-    def forward(self, x: torch.Tensor, final_activation=True):
+    def forward(self, x: torch.Tensor, final_activation=True) -> torch.Tensor:
         """
         Euler's method (ResNet with stride 1) for now.
 
@@ -37,21 +43,18 @@ class ODENet(nn.Module):
         Returns:
             (torch.Tensor): Output tensor
         """
-        x = torch.flatten(x)
-        for delta_t, layer in zip(self.delta_t, self.layers):
-            x = x + delta_t * torch.sigmoid(layer(x))
+        x = torch.flatten(x, 1)
+        for delta_t, layer in zip(self.delta_t, self.layers[:-1]):
+            x = x + delta_t * torch.relu(layer(x))
         x = self.layers[-1](x)
-        if not final_activation:
-            return x
-        return torch.sigmoid(x).view([])
+        return x.log_softmax(dim=1)
 
-    def train_network(self, x: torch.Tensor, y: torch.Tensor, epochs: int = 1, lr: float = 0.001, batch_size: int = 4):
+    def train_network(self, train_loader: DataLoader, epochs: int = 1, lr: float = 0.001, batch_size: int = 4):
         """
         Minimize loss function over weights of the network.
 
         Args:
-            x (torch.Tensor): Inputs
-            y (torch.Tensor): Outputs
+            train_loader (DataLoader): DataLoader for the training data
             epochs (int): Number of training iterations
             lr (float): The learning rate of the
 
@@ -61,30 +64,37 @@ class ODENet(nn.Module):
         device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         print(f'Using device \'{device}\' for training')
         self.to(device)
-        y = y.float()
 
-        loss_function = nn.MSELoss()
+        loss_function = nn.NLLLoss()
         optimizer = optim.Adam(self.parameters(), lr=lr)
         training_results = []
         for epoch in trange(epochs):
-            for i in range(x.size()[1]):
-                # Unpack data
-                input, label = x[:, i], y[i]
-
+            for i, batch in enumerate(train_loader):
+                x_batch, y_batch = map(lambda x: x.to(device), batch)
                 # Reset optimizer
                 optimizer.zero_grad()
 
                 # Forward, backward then optimize
-                outputs = self(input)
-                loss = loss_function(outputs, label)
+                # print(x_batch.shape, y_batch.shape)
+                outputs = self(x_batch)
+                loss = loss_function(outputs, y_batch)
                 loss.backward()
                 optimizer.step()
 
                 training_results.append(loss.item())
-                if not (i + 1) % 100:
-                    # print(f'Loss: {training_results[-1]}')
-                    pass
+        return training_results
 
-
+    def evaluate(self, test_loader: DataLoader):
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        test_cases = len(test_loader)
+        correct_predictions = 0
+        with torch.no_grad():
+            for x, y in test_loader:
+                prediction_values = self.forward(x.to(device))
+                prediction = np.argmax(prediction_values.cpu().numpy())
+                if prediction == y:
+                    correct_predictions += 1
+            accuracy = correct_predictions / test_cases
+            return accuracy
 
 
