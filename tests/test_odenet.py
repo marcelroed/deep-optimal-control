782a975bda34bbc374834692960e1ed4ea2a0eb7
marod <marod@stud.ntnu.no>
2019-11-05 16:07:28 +0100

    Test functions

--- tests/test_odenet.py
+++ tests/test_odenet.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
-from src.odenet.odenet import ODENet
+import torch
+from src.odenet.odenet import EulerNet
+from src.odenet.rk import RKLayer
 from src.data.load_data import get_mnist
 
 
@@ -10,5 +12,27 @@ def test_mnist():
 
     print(train.data[0])
 
-    net = ODENet()
+    net = EulerNet()
 
+
+def test_rk_layers():
+    l = RKLayer(2)
+    print(l.parameters)
+
+
+def test_delta_t_gradient():
+    net = EulerNet(2, 2)
+    inp = torch.Tensor([2., 2.])
+    outp = torch.Tensor([2., 3.])
+    print(net.delta_t)
+
+    # Train for one step
+    loss_func = torch.nn.MSELoss()
+    optimizer = torch.optim.SGD(net.parameters(), lr=1)
+    optimizer.zero_grad()
+    outp = net.forward(inp)
+    loss = loss_func(inp, outp)
+    loss.backward()
+    optimizer.step()
+
+    print(net.delta_t)
